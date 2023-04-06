fa49bf4058f6025113bb7f4e4ed386e46c1e3789
-V- <123872869+Armageddon47@users.noreply.github.com>
2023-04-06 16:23:39 +0330

    day 6 complete

--- main.py
+++ main.py
@@ -26,3 +26,31 @@ with open('input.txt') as f:
             break
 #####
 ####End of part 1
+largeIndex = 13
+smallIndex = 0
+isFound = False
+with open('input.txt') as f:
+    for i in f:
+        while(largeIndex < len(i)):
+            temparr1 = []
+            temparr1.extend(i[smallIndex:(largeIndex+1)])
+            temp1 = smallIndex
+            temp2 = largeIndex
+            while(temp1 <= temp2):
+                first_element = temparr1.pop(0)
+                if len(temparr1) > 0:
+                    if first_element in temparr1:
+                        break
+                temp1 += 1
+            if len(temparr1) == 0:
+                print(largeIndex+1)
+                isFound = True
+                break    
+      
+
+            largeIndex += 1
+            smallIndex += 1
+        if(isFound):
+            break
+        
+        ##end of part 2
